11cf6441728da320fed8b88c5595bbeaa61527a2
soonann <mail@soonann.dev>
2024-11-02 17:14:09 +0800

    chore: added api for realtime data

---
+++ app/api/route.ts
@@ -0,0 +1,13 @@
+export async function GET(request: Request) {
+
+  const res = await fetch(
+      'https://www.healthmap.org/getAlerts.php?category%5B%5D=1&category%5B%5D=2&category%5B%5D=29&diseases%5B%5D=82&time_interval=1+month&heatscore=1&partner=hm',
+      {
+          method: 'GET',
+      }
+  )
+ 
+  const data = await res.json()
+ 
+  return Response.json(data)
+}
---
+++ app/page.js
@@ -0,0 +1,65 @@
+'use client'
+import {
+    GoogleMap,
+    MarkerF,
+    useJsApiLoader,
+} from "@react-google-maps/api";
+import { useEffect, useState } from "react";
+
+
+
+export default function Home() {
+
+    // Map stuff
+    const defaultMapOptions = {
+        zoomControl: true,
+        tilt: 0,
+        gestureHandling: 'auto',
+        // mapTypeId: 'satellite',
+    };
+    const defaultMapZoom = 3
+    const defaultMapCenter = {
+        lat:28.2495102,
+        lng: 8.7116714,
+    }
+    const defaultMapContainerStyle = {
+       width: '100%',
+       height: '100vh',
+       // borderRadius: '15px 0px 0px 15px',
+    };
+
+    const [data, setData] = useState([])
+    const [isLoading, setLoading] = useState(true)
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API ,
+    });
+
+
+    useEffect(() => {
+            fetch('/api')
+            .then((res) => res.json())
+            .then((data) => {
+                    console.log(data.markers)
+                    setData(data.markers)
+                    setLoading(false)
+                    })
+            }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (data) return (
+        <div>
+             <GoogleMap
+                 mapContainerStyle={defaultMapContainerStyle}
+                 center={defaultMapCenter}
+                 zoom={defaultMapZoom}
+                 options={defaultMapOptions}
+             >
+                 { data.map((x ,i) => <MarkerF
+                     position={{ lat: x.lat, lng:x.lon }}
+                     key={i}
+                 ></MarkerF>) }
+             </GoogleMap>
+        </div>
+    )
+}
+
