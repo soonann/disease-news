afbcca6672e31277b8d76b27a5fda36e940104ce
soonann <mail@soonann.dev>
2024-11-09 16:20:31 +0800

    feat: cleaned up maps and table design

--- app/page.js
+++ app/page.js
@@ -10,14 +10,14 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
-    const ignoreCols = ['Source', 'Species', 'Disease']
+    const ignoreCols = ["Source", "Species", "Disease", "Deaths", "Cases"]
 
     // Map stuff
     const defaultMapOptions = {
         zoomControl: true,
         tilt: 0,
         gestureHandling: 'auto',
-        // mapTypeId: 'satellite',
+        // mapTypeId: 'hybrid',
     };
     const defaultMapZoom = 3
     const defaultMapCenter = {
@@ -26,12 +26,13 @@ export default function Home() {
     }
     const defaultMapContainerStyle = {
         width: '100%',
-        height: '100vh',
+        height: '100%',
         // borderRadius: '15px 0px 0px 15px',
     };
 
     const [data, setData] = useState([])
     const [isLoading, setLoading] = useState(true)
+    const [active, setActive] = useState(-1)
     const { isLoaded } = useJsApiLoader({
         googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API,
     });
@@ -47,10 +48,16 @@ export default function Home() {
             })
     }, [])
 
+    let onMarkerClick = (e, i) => {
+        // console.log(e)
+        // console.log(i)
+        setActive(i)
+    }
+
     if (isLoading && !isLoaded) return <p>Loading...</p>
     if (data && data.markers) return (
-        <div className="flex flex-row">
-            <div className="basis-2/3">
+        <div className="grid grid-cols-3 gap-2 p-2 h-full justify-items-stretch">
+            <div className="col-span-2 h-full">
                 <GoogleMap
                     mapContainerStyle={defaultMapContainerStyle}
                     center={defaultMapCenter}
@@ -59,17 +66,30 @@ export default function Home() {
                 >
                     {
                         data.markers.map((x, i) =>
-                            <MarkerF position={{
-                                lat: x.lat,
-                                lng: x.lon
-                            }} key={i} />)
+                            <MarkerF
+
+                                position={{
+                                    lat: x.lat,
+                                    lng: x.lon
+                                }}
+                                icon={{
+                                    path: google.maps.SymbolPath.CIRCLE,
+                                    scale: 8.5,
+                                    fillColor: "purple",
+                                    fillOpacity: 0.5,
+                                    strokeWeight: 0.2,
+                                }}
+                                key={i}
+                                onClick={(e) => { onMarkerClick(e, i) }}
+                            />)
                     }
                 </GoogleMap>
             </div>
-            <div className="basis-1/3 overflow-y-scroll max-h-lvh">
-                <table border={1}>
-                    <tbody>
-                        <tr>
+            <div className="col-span-1 overflow-y-scroll h-full">
+                <table className="border h-full w-full">
+                    <tbody >
+                        <tr className="text-xs"
+                        >
                             {
                                 data.list_view_header.map(
                                     (v, k) =>
@@ -78,6 +98,7 @@ export default function Home() {
                                             :
                                             <td
                                                 key={k + v}
+                                                className="border"
                                             >
                                                 {v}
                                             </td>
@@ -87,7 +108,10 @@ export default function Home() {
                         {
                             // For each news value in the listview
                             data.listview.map((row_v, row_i) =>
-                                <tr key={row_i}>
+                                <tr
+                                    key={row_i}
+                                    className={"text-xs" + (row_i == active ? " bg-purple-300" : "")}
+                                >
                                     {
                                         // Create all the cells
                                         data.list_view_header.map((cell_v, cell_i) =>
@@ -96,7 +120,8 @@ export default function Home() {
                                                 :
                                                 <td
                                                     key={row_i + "-" + cell_i}
-                                                    dangerouslySetInnerHTML={{ __html: row_v[cell_i] }}
+                                                    className="border"
+                                                    dangerouslySetInnerHTML={{ __html: row_v[cell_i] ?? 0 }}
                                                 />
                                         )
                                     }
@@ -104,6 +129,10 @@ export default function Home() {
                         }
                     </tbody>
                 </table>
+            </div>
+
+            <div>
+
             </div>
         </div>
     )
