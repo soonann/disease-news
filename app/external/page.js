b404933cc3c285c177ba5b7c90a0ec51bb8eb596
soonann <mail@soonann.dev>
2024-11-14 17:31:25 +0800

    chore: updated dashboards

--- app/external/page.js
+++ app/external/page.js
@@ -2,7 +2,7 @@
 import TableauEmbed from '../components/TableauEmbed';
 
 const HomePage = () => {
-    const tableauUrl = 'https://public.tableau.com/views/external-factors/FINAL?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link&:device=desktop';
+    const tableauUrl = 'https://public.tableau.com/views/measles_external-factors/FINAL?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link&:device=desktop';
 
     return (
         <div>
--- app/macro/page.js
+++ app/macro/page.js
@@ -3,10 +3,16 @@ import TableauEmbed from '../components/TableauEmbed';
 
 
 const HomePage = () => {
-    const tableauUrl = 'https://public.tableau.com/views/MacroAnalysis_17315685832090/MacroAnalysisDashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link&:device=desktop';
+    const tableauUrl2 = 'https://public.tableau.com/views/MacroAnalysis_17315685832090/MacroAnalysisDashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link&:device=desktop';
+    const tableauUrl = 'https://public.tableau.com/views/DashboardNumberofMeaslesCasesReportedGlobally/FinalDashboard?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link&:device=desktop';
 
     return (
-        <TableauEmbed url={tableauUrl} />
+
+        <div>
+            <TableauEmbed url={tableauUrl} />
+            <br />
+            <TableauEmbed url={tableauUrl2} />
+        </div>
     );
 };
 
